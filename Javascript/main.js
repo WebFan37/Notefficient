a993fca0f8788db3bbc50ecc5193bbdb1308f4f2
WebFan37 <118756551+WebFan37@users.noreply.github.com>
2025-02-06 08:15:57 -0800

    initialiser les animations (phase débutant)

--- Javascript/main.js
+++ Javascript/main.js
@@ -6,22 +6,27 @@ window.onscroll = () => {
     sections.forEach(sec => {
 
         //Get the top of the section
-        const top = window.scrollY;
+        const distanceScroll = window.scrollY;
 
         //Get the height of the section
-        const height = sec.offsetHeight;
+        const sectionHeight = sec.offsetHeight;
 
         //Savoir notre offset top
-        const offset = sec.offsetTop;
+        const offset = sec.offsetTop - 150;
 
         //Window height
         const windowH = window.innerHeight;
         
-        
-        // console.log(top);
-        //  console.log(height);
-        //  console.log(offset);
+    
 
+        // Si on est dans la section, ajouter classe animation
+        if (distanceScroll >= offset && distanceScroll < offset + sectionHeight ){
+            sec.classList.add('donnerAnimation');
+            // console.log("On est dans la section")
+        }else{
+            sec.classList.remove('donnerAnimation');
+            // console.log("On est pas dans la section")
+        }
         
     })
 }
