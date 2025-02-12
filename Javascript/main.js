7c1b5d012d0a75bea9ec897ba47bb3ac3b89c5dc
WebFan37 <118756551+WebFan37@users.noreply.github.com>
2025-02-12 13:24:09 -0800

    Animation Section Design moitie reussi

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,32 +1,76 @@
-//GRab toutes les sections main pour preparer l'animation
-const sections = document.querySelectorAll('main');
+//==========================PAGE INTRO =====================//
+//===========================================================//
 
+
+//GRab la section pour preparer l'animation
+const UINote = document.querySelector('.UI-Note');
+
+const divNote = document.querySelector('.UI-Note div');
 //Scroll on animation for each section
 window.onscroll = () => {
-    sections.forEach(sec => {
 
         //Get the top of the section
-        const distanceScroll = window.scrollY;
+        const distanceScrollUI = window.scrollY;
 
         //Get the height of the section
-        const sectionHeight = sec.offsetHeight;
+        const heightNote = UINote.offsetHeight;
 
         //Savoir notre offset top
-        const offset = sec.offsetTop - 150;
+        const offsetUI = UINote.offsetTop - 150;
 
-        //Window height
-        const windowH = window.innerHeight;
         
     
 
         // Si on est dans la section, ajouter classe animation
-        if (distanceScroll >= offset && distanceScroll < offset + sectionHeight ){
-            //sec.classList.add('donnerAnimation');
-            // console.log("On est dans la section")
+        if (distanceScrollUI >= offsetUI && distanceScrollUI < offsetUI + heightNote ){
+            divNote.classList.add('donnerAnimation');
+           
         }else{
-            //sec.classList.remove('donnerAnimation');
-            // console.log("On est pas dans la section")
+            divNote.classList.remove('donnerAnimation');
+
         }
         
-    })
+
+}
+
+
+//==========================PAGE DESIGN =====================//
+//===========================================================//
+//Grab 2e section
+const sectionDesign = document.querySelector('.Design');
+
+//Grab les paragraphes et h2 pour animation 
+const h2 = document.querySelector('.Design div h2');
+const paragraphes = document.querySelectorAll('.Design div p');
+
+//grabd l'image
+const image = document.querySelector('.Design img');
+
+//Scroll on animation on Design
+window.onscroll = () => {
+    const distanceScroll = window.scrollY;
+    const heightDesign = sectionDesign.offsetHeight;
+    const offset = sectionDesign.offsetTop - 150;
+   
+
+    // Si on est dans la section, ajouter classe animation
+    if (distanceScroll >= offset && distanceScroll < offset + heightDesign ){
+        h2.classList.add('donnerAnimationH2');
+
+        image.classList.add('AnimationImg');
+
+        paragraphes.forEach(paragr => {
+            paragr.classList.add("donnerAnimationPar")
+        })
+        
+    }else{
+        h2.classList.remove('donnerAnimationH2');
+
+        image.classList.remove('AnimationImg');
+
+        paragraphes.forEach(paragr => {
+            paragr.classList.remove("donnerAnimationPar")
+        })
+      
+    }
 }
