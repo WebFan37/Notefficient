b665968e95e7b3a29a2c4d5b773e0aeb74afe11a
WebFan37 <118756551+WebFan37@users.noreply.github.com>
2025-02-14 12:20:29 -0800

    video demo apply and code cleaning by chatGPT

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,120 +1,71 @@
-//==========================PAGE INTRO =====================//
-//===========================================================//
-
-
-//GRab la section pour preparer l'animation
-const UINote = document.querySelector('.UI-Note');
-
-const divNote = document.querySelector('.UI-Note div');
-//Scroll on animation for each section
+//========================== GLOBAL SCROLL EVENT =====================//
 window.onscroll = () => {
+    handleScrollNote();
+    handleScrollDesign();
+    handleScrollUI();
+};
 
-        //Get the top of the section
-        const distanceScrollNote = window.scrollY;
+//========================== PAGE INTRO (NOTE) =====================//
+const UINote = document.querySelector('.UI-Note');
+const divNote = document.querySelector('.UI-Note div');
 
-        //Get the height of the section
-        const heightNote = UINote.offsetHeight;
+function handleScrollNote() {
+    const scrollY = window.scrollY;
+    const offsetUI = UINote.offsetTop - 150;
+    const heightNote = UINote.offsetHeight;
 
-        //Savoir notre offset top
-        const offsetUI = UINote.offsetTop - 150;
-
-        
-    
-
-        // Si on est dans la section, ajouter classe animation
-        if (distanceScrollNote >= offsetUI && distanceScrollNote < offsetUI + heightNote ){
-            divNote.classList.add('donnerAnimation');
-           
-        }else{
-            divNote.classList.remove('donnerAnimation');
-
-        }
-        
-
+    if (scrollY >= offsetUI && scrollY < offsetUI + heightNote) {
+        divNote.classList.add('donnerAnimation');
+    } else {
+        divNote.classList.remove('donnerAnimation');
+    }
 }
 
-
-//==========================PAGE DESIGN =====================//
-//===========================================================//
-//Grab 2e section
+//========================== PAGE DESIGN =====================//
 const sectionDesign = document.querySelector('.Design');
-
-//Grab les paragraphes et h2 pour animation 
 const h2 = document.querySelector('.Design div h2');
 const paragraphes = document.querySelectorAll('.Design div p');
-
-//grabd l'image
 const image = document.querySelector('.Design img');
 
-//Scroll on animation on Design
-window.onscroll = () => {
-    const distanceScroll = window.scrollY;
-    const heightDesign = sectionDesign.offsetHeight;
+function handleScrollDesign() {
+    const scrollY = window.scrollY;
     const offset = sectionDesign.offsetTop - 150;
-   
+    const heightDesign = sectionDesign.offsetHeight;
 
-    // Si on est dans la section, ajouter classe animation
-    if (distanceScroll >= offset && distanceScroll < offset + heightDesign ){
+    if (scrollY >= offset && scrollY < offset + heightDesign) {
         h2.classList.add('donnerAnimationH2');
-
         image.classList.add('AnimationImg');
 
-        paragraphes.forEach(paragr => {
-            paragr.classList.add("donnerAnimationPar")
-        })
-        
-    }else{
+        paragraphes.forEach(paragr => paragr.classList.add("donnerAnimationPar"));
+    } else {
         h2.classList.remove('donnerAnimationH2');
-
         image.classList.remove('AnimationImg');
 
-        paragraphes.forEach(paragr => {
-            paragr.classList.remove("donnerAnimationPar")
-        })
-      
+        paragraphes.forEach(paragr => paragr.classList.remove("donnerAnimationPar"));
     }
 }
 
-
-//==========================PAGE UI =====================//
-//===========================================================//
-
-//Grab la section UI
+//========================== PAGE UI =====================//
 const sectionUI = document.querySelector('.UI-Description');
-
-//Grab les images pour animation
 const images = document.querySelectorAll('.UI-Description img');
 
-
-window.onscroll = () => {
-    const distanceScrollUI = window.scrollY;
-    const heightUI = sectionUI.offsetHeight;
+function handleScrollUI() {
+    const scrollY = window.scrollY;
     const offsetUI = sectionUI.offsetTop - 150;
+    const heightUI = sectionUI.offsetHeight;
+    const pointAnimation = offsetUI + heightUI * 0.5; // 50% of the section
 
-    
-
-
-    // Si on est dans la section, ajouter classe animation (BASE)
-    if (distanceScrollUI >= offsetUI && distanceScrollUI < offsetUI + heightUI){
+    // Image animation
+    if (scrollY >= offsetUI && scrollY < offsetUI + heightUI) {
         images.forEach((img, index) => {
-            
-            setTimeout(() => {img.classList.add('donnerAnimationUI')}, index * 400)
-            
-        } )
+            setTimeout(() => img.classList.add('donnerAnimationUI'), index * 400);
+        });
     } else {
-        images.forEach(img => {
-            img.classList.remove('donnerAnimationUI')
-        } )
+        images.forEach(img => img.classList.remove('donnerAnimationUI'));
     }
 
-    //Nouveau ! ====//
-    //En défilant 50% de la section on ajoute une autre classe pour rentrer les paragraphes
-    const pointAnimation = offsetUI + heightUI * 0.5;
-
-    //Nouveau ! ====//
-    //Ajouter une autre classe pour introduire les paragraphes apres 50% de la section
-    if (distanceScrollUI >= pointAnimation && distanceScrollUI <= offsetUI + heightUI){
-        console.log("C'est le moment pour ajouter les paragraphes !")
-        
+    // Paragraph introduction at 50% scroll
+    if (scrollY >= pointAnimation && scrollY <= offsetUI + heightUI) {
+        console.log("C'est le moment pour ajouter les paragraphes !");
     }
 }
